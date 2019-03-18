684761a4e258ef49a8d2deefc92bc79c4963bd57
Liam Stewart <15363506+liamstewart23@users.noreply.github.com>
2019-03-18 18:38:48 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,19 +3,10 @@
 Plugin Name: Force Delete Posts
 Plugin URI: https://github.com/liamstewart23/WordPressForceDeletePosts
 Description: Adds the ability for administrators to instantly delete posts by skipping the trash.
-Version: 1.0.0
+Version: 1.1.0
 Author: Liam Stewart
 Author URI: https://liamstewart.ca
 */
-
-/**
- * WordPress Version 4.0 or greater
- */
-$requiredVersion = "4.0";
-if (version_compare(get_bloginfo('version'), $requiredVersion, '<')) {
-    wp_die("<h1>You must update WordPress to use this plugin! </h1><br>
-    You are currently running WordPress version <strong>" . get_bloginfo('version') . "</strong><br> This plugin requires <strong>" . $requiredVersion . "</strong> or greater");
-}
 
 /**
  * Post List Styles
@@ -24,7 +15,6 @@ function ls_fd_column_width()
 {
     echo '<style type="text/css">';
     echo '.column-ls_fd_column { text-align: center; width:60px !important; overflow:hidden }';// Custom Column styles
-    echo '.ls_plfi_icon {height:12px;width:12px;border-radius:50%;}';// Circle icon styles
     echo '.ls_plfi_label {justify-content:center;display:flex;}';
     echo '</style>';
 }
@@ -49,7 +39,8 @@ function ls_fd_columns_head($defaults)
  */
 function ls_fd_columns_content($column_name, $post_ID)
 {
-    if ($column_name === 'ls_fd_column') {// If column exists
+    // If column exists
+    if ($column_name === 'ls_fd_column') {
         ls_fd_force_delete('<span class="dashicons dashicons-trash"></span>');
     }
 }
